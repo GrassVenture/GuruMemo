b903a0930a471598cdd5025d1a544d2f69308899
sasatank <sasa.tankytank@gmail.com>
2025-01-22 22:36:23 +0900

    fix 写真分類タブを削除

--- lib/core/router.dart
+++ lib/core/router.dart
@@ -11,8 +11,6 @@ import '../features/photo/camera/camera_detail_page.dart';
 import '../features/photo/camera/camera_page.dart';
 import '../features/photo/gallery/gallery_page.dart';
 import '../features/photo/photo_detail/photo_detail_page.dart';
-import '../features/photo/swipe_photo/classify_start_page.dart';
-import '../features/photo/swipe_photo/swipe_photo_page.dart';
 import '../features/root_page.dart';
 import 'services/analytics_service.dart';
 
@@ -42,16 +40,6 @@ final routerProvider = Provider(
             name: CameraPage.routeName,
             path: CameraPage.routePath,
             builder: (context, state) => const CameraPage(),
-          ),
-          GoRoute(
-            name: ClassifyStartPage.routeName,
-            path: ClassifyStartPage.routePath,
-            builder: (context, state) => const ClassifyStartPage(),
-          ),
-          GoRoute(
-            name: SwipePhotoPage.routeName,
-            path: SwipePhotoPage.routePath,
-            builder: (context, state) => const SwipePhotoPage(),
           ),
         ],
       ),
