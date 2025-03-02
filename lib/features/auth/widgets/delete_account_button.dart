061157f0152d2852bbb1fe69cd7bfb447da0e782
masaki <masakisato.japan@gmail.com>
2025-03-02 12:03:59 +0900

    コンフリクト解消後に実装を反映

--- lib/features/auth/widgets/delete_account_button.dart
+++ lib/features/auth/widgets/delete_account_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../../core/services/analytics_service.dart';
@@ -6,6 +7,7 @@ import '../../../core/themes.dart';
 import '../../../core/widgets/app_dialog.dart';
 import '../../../core/widgets/app_snack_bar.dart';
 import '../auth_controller.dart';
+import '../sign_in_page.dart';
 
 class DeleteAccountButton extends ConsumerWidget {
   const DeleteAccountButton({super.key});
@@ -29,6 +31,10 @@ class DeleteAccountButton extends ConsumerWidget {
                 ref
                     .read(analyticsServiceProvider)
                     .sendEvent(name: 'delete_account');
+                if (!context.mounted) {
+                  return;
+                }
+                context.go(SignInPage.routePath);
                 AppSnackBar.show(message: 'アカウントを削除しました');
               },
             );
