b8ba0c7728c9cb1bb139b6a28d183f2e1ef9aaf7
sasatank <sasa.tankytank@gmail.com>
2025-03-07 23:04:25 +0900

    feature SelectedIndexをnotifierProviderに変更

---
+++ lib/core/permission/permission_handler.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'permission_handler.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$permissionHandlerHash() => r'88da5a1facf0e99394adff8ebbd9273a5e780c61';
+
+/// See also [permissionHandler].
+@ProviderFor(permissionHandler)
+final permissionHandlerProvider =
+    AutoDisposeProvider<PermissionHandler>.internal(
+  permissionHandler,
+  name: r'permissionHandlerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$permissionHandlerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef PermissionHandlerRef = AutoDisposeProviderRef<PermissionHandler>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
